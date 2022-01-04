84509b10b4a925a04d7e73a9e442f88af63a232e
Al Amin <alamin@repto.co>
2022-01-04 23:51:43 +0600

    Leetcode: 0001 Two sum problem solved in Java. With one for loop and Map.

---
+++ LeetCode0001.java
@@ -0,0 +1,21 @@
+package com.example.leetcode_problem_solving;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode0001 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                result[0] = i;
+                result[1] = map.get(nums[i]);
+            }else{
+                map.put(target-nums[i], i);
+            }
+        }
+
+        return result;
+    }
+}
